788ed46a85a921f5f75c6e9b975d2993fc60834c
philipp <philipp@PhD-Mac.local>
2015-07-18 13:11:25 -0700

    includes json wordcounts also have surrounding words metric update

--- Acronym_Workspace/Wiki_Visit_Data/surrounding_words_metrics.py
+++ Acronym_Workspace/Wiki_Visit_Data/surrounding_words_metrics.py
@@ -17,37 +17,41 @@ compare_to_dict = {"words" : count}
 
 english_lang_freq : frequency of words in the english language
 """
+import copy
 import math
 import json
+import matplotlib.pyplot as plt
 
+def AddCounts(my_dict_cout):
+    count = 0
+    for value in my_dict_cout.itervalues():
+        count += value
+    return count
+
+def MapCountToFreq(english_lang_freq, my_englang_totalcount):
+    for key, value in english_lang_freq.iteritems():
+        english_lang_freq[key] *= 1.0 / (my_englang_totalcount)
+    return
 
 def FequenciesToInformation(in_dictionary_freq):
-    """
-    This function takes the probabilites and maps them to information content.
-    """
-    for key,value in english_lang_freq.iteritems():
-        value = math.log (1.0 / value)
-    return in_dictionary_info
+    for key, value in in_dictionary_freq.iteritems():
+        in_dictionary_freq[key] = math.log (1.0 / value)
+    return
 
-def CountsToFrequencies(in_dictionary_counts):
-    for pair in english_lang_freq:
-        in_dictionary_frequs = in_dictionary_counts / nr_of_words
-
-
-
-def WeightWordsFrequecyByInformation(my_dict_info, english_lang_info):
-    for key, value in my_dict:
-        value abs(my_dict_info - english_lang_info)
-
+#def WeightWordsFrequecyByInformation(my_dict_info, english_lang_info):
+#    for key, value in my_dict:
+#        value abs(my_dict_info - english_lang_info)
+#
 
 # Considerations cutoff of noise
 
+# No stopwords -- no meaning attached to them
 
 # Weighted: Rare words produce less of an impact than common words 
 def SimplestMetricWighted(my_dict_freq, compare_to_dict_freq):
     metric = 0.0
     for key, value in my_dict_freq.iteritems():
-        metric += value  * abs(log_2(compare_to_dict_freq[key] / value)) * (1.0 / english_lang_info[key])
+        metric += abs(compare_to_dict_freq[key] * value) * (1.0 / english_lang_info[key])
     return metric
     
 # No weighting in the one below. Sort according to lowest ones
@@ -57,16 +61,29 @@ def SimplestMetric(my_dict_freq, compare_to_dict_freq):
         metric += value  * abs(log_2(compare_to_dict_freq[key] / value))
     return metric 
      
-def PlotWordDistributionOfText():
-    suff
+def PlotKeysHistogram():
     
     
+
+# Plot: def information content of words
 # -----
     
 def main():
-    fp_englang_json = open()
-    my_englang = json.load(fp_englang_json) 
+    fp_englang_json = open('../Word_Frequency/Word_Count/ANC_count.json')
+    my_englang_count = json.load(fp_englang_json) 
     fp_englang_json.close()
-    print fp_englang_json
+    my_englang_totalcount = AddCounts(my_englang_count)
+    print my_englang_totalcount
+    my_englang_freq = copy.deepcopy(my_englang_count)
+    MapCountToFreq(my_englang_freq, my_englang_totalcount)
+    print my_englang_count["test"]
+    print my_englang_freq["test"]
+    my_englang_info = copy.deepcopy(my_englang_freq)
+    FequenciesToInformation(my_englang_info)
+    print my_englang_info.values()
+    
+    PlotKeysHistogram(my_englang_info);
+    
+
     
 if __name__ == "__main__": main()
